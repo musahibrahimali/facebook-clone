07bb2494e98c7756485a685f758c2ab35ab9a9eb
Musah Ibrahim Ali <miastudiosinc@gmail.com>
2021-06-10 01:40:08 +0000

    all set

---
+++ src/Config/firebase.jsx
@@ -0,0 +1,18 @@
+import firebase from 'firebase';
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAq-KBtyRr2bIQtYeK_iDVInXQH6-IzaRE",
+    authDomain: "facebook-clone-mia.firebaseapp.com",
+    projectId: "facebook-clone-mia",
+    storageBucket: "facebook-clone-mia.appspot.com",
+    messagingSenderId: "179887170629",
+    appId: "1:179887170629:web:647074cd3e63706dd6ef2c",
+    measurementId: "G-61ZWBNNMDE"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const database = firebaseApp.firestore();
+const authentication = firebaseApp.auth();
+
+export { database, authentication };
