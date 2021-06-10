f300a31937057eb108dc2d88d4984017c45c7183
Musah Ibrahim Ali <miastudiosinc@gmail.com>
2021-06-10 02:43:16 +0000

    adding some files

---
+++ src/Components/exports.jsx
@@ -0,0 +1,4 @@
+export { default as Header } from './Header/Header';
+export { default as Login } from './Login/Login';
+export { default as SideBar } from './SideBar/SideBar';
+export { default as Feed } from './Feed/Feed';
